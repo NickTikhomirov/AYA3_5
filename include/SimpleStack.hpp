6a28de0cb68de2f9b9853cdf3a563da382680272
NickTikhomirov <nicktikhomirov02@gmail.com>
2019-11-24 23:28:28 +0300

    simple1 initial

---
+++ include/SimpleStack.hpp
@@ -0,0 +1,77 @@
+// Copyright 2018 Your Name <your_email>
+
+#ifndef INCLUDE_HEADER_HPP_
+#define INCLUDE_HEADER_HPP_
+
+#include <utility>
+
+
+
+template <typename T>
+class SimpleStack
+{
+	struct Node {
+		T data;
+		Node* next = nullptr;
+		Node(T& arg) { data = arg; }
+		Node(T&& arg) {	data = std::move(arg); }
+		static Node* create(const T& arg, Node* pointer) {
+			Node* n = new Node(arg);
+			n->next = pointer;
+			return n;
+		}
+		static Node* create(T&& arg, Node* pointer) {
+			Node* n = new Node(std::move(arg));
+			n->next = pointer;
+			return n;
+		}
+		~Node() {
+			if (next != nullptr) {
+				delete next;
+				next = nullptr;
+			}
+		}
+	};
+	SimpleStack(const SimpleStack& a) {}
+	void operator=(const SimpleStack& a) {}
+	Node* top = nullptr;
+
+
+public:
+	SimpleStack() {}
+	SimpleStack(T&& value) {
+		push(std::move(value));
+	}
+	SimpleStack(const T& value) {
+		push(value);
+	}
+	SimpleStack(SimpleStack&& r) {
+		top = r.top;
+		r.top = nullptr;
+	}
+	void operator=(SimpleStack&& r) {
+		top = r.top;
+		r.top = nullptr;
+	}
+	void push(T&& value) { top = Node::create(std::move(value), top);}
+	void push(const T& value) {	top = Node::create(value, top);}
+	void pop() {
+		Node* temp = top;
+		top = temp->next;
+		temp->next = nullptr;
+		delete temp;
+	}
+	const T& head() const {	
+		if (top == nullptr) return 0;
+		return top->data;
+	}
+	~SimpleStack() {
+		if (top != nullptr) {
+			delete top;
+			top = nullptr;
+		}
+	}
+};
+
+
+#endif // INCLUDE_HEADER_HPP_
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,14 @@
 // Copyright 2018 Your Name <your_email>
 
 #include <gtest/gtest.h>
+#include "SimpleStack.hpp"
 
-TEST(Example, EmptyTest) {
-    EXPECT_TRUE(true);
+TEST(Stack1_base, push_pop) {
+	SimpleStack<int> s;
+	s.push(1);
+	EXPECT_EQ(s.head(), 1);
+	s.push(3);
+	EXPECT_EQ(s.head(), 3);
+	s.pop();
+	EXPECT_EQ(s.head(), 1);
 }
